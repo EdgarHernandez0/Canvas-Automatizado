7aaaddd1f3575e2934bbba08150346b7f93b5d5f
Karla Garcia <kygarcias@outlook.com>
2022-11-16 17:01:03 -0600

    Tooltip en Ventana modal
    Desaparecer tooltip de la ventana modal al ingresar información

--- JS/main.js
+++ JS/main.js
@@ -5,13 +5,13 @@ const closeModal =document.querySelector('.modal_close');
 openModal.addEventListener('click', (e)=>{
     e.preventDefault();
     modal.classList.add('modal--show');
-
+    tooltipOcultar();
 });
 
 closeModal.addEventListener('click', (e)=>{
     e.preventDefault();
     modal.classList.remove('modal--show');
-
+    tooltipMostrar();
 });
 
 const formulario_obj = document.querySelector('#form_obj');
@@ -35,3 +35,43 @@ formulario_vis.addEventListener("submit", ()=>{
 
     modal.classList.remove('modal--show');
 });
+
+function tooltipOcultar(){
+    document.querySelector('#tooltipEle1').style.visibility = "hidden";
+    document.querySelector('#tooltipEle2').style.visibility = "hidden";
+    document.querySelector('#tooltipEle3').style.visibility = "hidden";
+    document.querySelector('#tooltipEle4').style.visibility = "hidden";
+    document.querySelector('#tooltipEle5').style.visibility = "hidden";
+    document.querySelector('#tooltipEle6').style.visibility = "hidden";
+    document.querySelector('#tooltipEle7').style.visibility = "hidden";
+    document.querySelector('#tooltipEle8').style.visibility = "hidden";
+    document.querySelector('#tooltipEle9').style.visibility = "hidden";
+    document.querySelector('#tooltipEle10').style.visibility = "hidden";
+    document.querySelector('#tooltipEle11').style.visibility = "hidden";
+    document.querySelector('#tooltipEle12').style.visibility = "hidden";
+    document.querySelector('#tooltipEle13').style.visibility = "hidden";
+    document.querySelector('#tooltipEle14').style.visibility = "hidden";
+    document.querySelector('#tooltipEle15').style.visibility = "hidden";
+    document.querySelector('#tooltipEle16').style.visibility = "hidden";
+    document.querySelector('#tooltipEle17').style.visibility = "hidden";
+}
+
+function tooltipMostrar(){
+    document.querySelector('#tooltipEle1').style.visibility = "visible";
+    document.querySelector('#tooltipEle2').style.visibility = "visible";
+    document.querySelector('#tooltipEle3').style.visibility = "visible";
+    document.querySelector('#tooltipEle4').style.visibility = "visible";
+    document.querySelector('#tooltipEle5').style.visibility = "visible";
+    document.querySelector('#tooltipEle6').style.visibility = "visible";
+    document.querySelector('#tooltipEle7').style.visibility = "visible";
+    document.querySelector('#tooltipEle8').style.visibility = "visible";
+    document.querySelector('#tooltipEle9').style.visibility = "visible";
+    document.querySelector('#tooltipEle10').style.visibility = "visible";
+    document.querySelector('#tooltipEle11').style.visibility = "visible";
+    document.querySelector('#tooltipEle12').style.visibility = "visible";
+    document.querySelector('#tooltipEle13').style.visibility = "visible";
+    document.querySelector('#tooltipEle14').style.visibility = "visible";
+    document.querySelector('#tooltipEle15').style.visibility = "visible";
+    document.querySelector('#tooltipEle16').style.visibility = "visible";
+    document.querySelector('#tooltipEle17').style.visibility = "visible";
+}
